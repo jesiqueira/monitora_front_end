849060ce080c361c2af0dc50ddce18a823ef5c82
José Edmar de Siqueira <edmar.ade@gmail.com>
2023-12-19 07:21:05 -0300

    finalizado css para paginação

--- src/Components/Helper/Paginacao.jsx
+++ src/Components/Helper/Paginacao.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Paginacao.module.css'
 
 const Paginacao = ({ totalItems, itemsPorPagina, onPageChange }) => {
   const [currentPage, setCurrentPage] = React.useState(1)
@@ -18,10 +19,10 @@ const Paginacao = ({ totalItems, itemsPorPagina, onPageChange }) => {
     }
   }
   return (
-    <div>
-      <ul className="pagination">
+    <div className={styles.container}>
+      <ul>
         {Array.from({ length: totalPages > 6 ? 6 : totalPages }, (_, index) => (
-          <li key={index} className={currentPage === index + 1 ? 'active' : ''}>
+          <li key={index} className={currentPage === index + 1 ? styles.active : ''}>
             <button onClick={() => handlePageClick(index + 1)} disabled={currentPage === index + 1}>
               {index + 1}
             </button>
