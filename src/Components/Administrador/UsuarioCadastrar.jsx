d4758cfd559309b01da4ebe7710b8fe2a120c70a
José Edmar de Siqueira <edmar.ade@gmail.com>
2023-12-31 09:27:43 -0300

    finalizado tela de cadastro de usuário com css

--- src/Components/Administrador/UsuarioCadastrar.jsx
+++ src/Components/Administrador/UsuarioCadastrar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './UsuarioCadastrar.module.css'
 import { UserContext } from '../../Contexts/UserContext'
 import FecharMenu from '../Helper/FecharMenu'
@@ -6,25 +7,115 @@ import { ReactComponent as Addusers } from '../../Assets/add-user.svg'
 import { ReactComponent as Detalhe } from '../../Assets/detalhe.svg'
 import Input from '../Forms/Input'
 import Button from '../Forms/Button'
+import Select from '../Forms/Select'
+import useColaborador from '../../Hooks/useColaboradorForm'
+import { getLocalSites } from '../../services/api/localSite'
+import { create } from '../../services/api/usuario/api'
 
 const UsuarioCadastrar = () => {
   const { setMenuadmin, setMenusair } = React.useContext(UserContext)
   const menuClose = [setMenusair, setMenuadmin]
+  const [select, setSelect] = React.useState('')
+  const [locais, setLocal] = React.useState(null)
+  const [isChecked, setChecked] = React.useState(false)
+  const [error, setError] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const [user, setUser] = React.useState({})
+  const navigate = useNavigate()
+
+  const nome = useColaborador('nome')
+  const login = useColaborador('login')
+  const senha = useColaborador('senha')
+
+  React.useEffect(() => {
+    const buscar = async () => {
+      setError(null)
+      try {
+        const response = await getLocalSites()
+        if (response.status === 200) {
+          setLocal(response.data)
+        } else {
+          throw new Error(response.error)
+        }
+      } catch (error) {
+        setError(error)
+      } finally {
+        setError(null)
+      }
+    }
+    buscar()
+  }, [])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const response = await create(select, user)
+        if (response.status === 201) {
+          navigate('/admin')
+        }
+      } catch (error) {
+        if (error.response.status === 400) {
+          setError(error.response.data.error)
+        } else if (error.response.status === 500) {
+          setError(error.response.data.error)
+        } else {
+          setError('Ocorreu um erro do nosso lado, contate o suporte ou desenvolvedor!')
+        }
+      } finally {
+        setLoading(false)
+        setError(null)
+      }
+    }
+    if (user.nome && user.login && user.senha_virtual) {
+      fetchData()
+    }
+  }, [user, navigate, select])
+
+  const handleCheckboxChange = () => {
+    setChecked((prevChecked) => !prevChecked)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setError(null)
+    if (nome.validate() && login.validate() && senha.validate() && select) {
+      setUser((user) => ({
+        ...user,
+        nome: nome.value,
+        login: login.value.toLowerCase(),
+        senha_virtual: senha.value,
+        is_admin: isChecked,
+      }))
+    } else if (select === '') {
+      setError('Selecione o local')
+    }
+  }
+
   return (
     <div className={styles.estrutura}>
       <FecharMenu menuToClose={menuClose} />
       <div className={styles.detalhe}>
         <Detalhe />
       </div>
-      <h1>Cadastro de Usuário</h1>
+      <div className={styles.titulo}>
+        <h1>Cadastro de Usuário</h1>
+      </div>
       <article className={styles.article}>
         <Addusers />
-        <form className={styles.form}>
-          <Input label="nome" type="text" placeholder="Name do usuário" />
-          <Input label="login" />
-          <Input label="password" type="password" />
-          <Input label="local" />
-          <Button>Cadastrar</Button>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Input label="nome" type="text" placeholder="Name do usuário" {...nome} />
+          <div className={styles.conta}>
+            <Input label="login" placeholder="informe login" {...login} />
+            <div className={styles.toggleContainer}>
+              <input className={styles.toggle} id="toggle" type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+              <label htmlFor="toggle" className={styles.toggleLabel}></label>
+              <span className={styles.description}>{isChecked ? 'Conta administrador' : 'Conta normal'}</span>
+            </div>
+          </div>
+          <Input label="Senha" type="password" placeholder="informe a senha" {...senha} />
+          <Select labelName="Local" name="local" select={select} setSelect={setSelect} locais={locais} error={error} />
+          <div className={styles.button}>{loading ? <Button disabled>Cadastrando....</Button> : <Button>Cadastrar</Button>}</div>
         </form>
       </article>
     </div>
--- src/services/api/usuario/api.js
+++ src/services/api/usuario/api.js
@@ -18,6 +18,24 @@ export const getUsuarios = async () => {
   }
 }
 
+export const create = async (idSite, user) => {
+  try {
+    let url = `/site/${idSite}/users`
+    return api.post(url, user)
+  } catch (error) {
+    if (error.response) {
+      console.error('Response error:', error.response.status)
+      console.error(error.response.data)
+    } else if (error.request) {
+      console.error('Request error:', error.request)
+    } else {
+      console.error('Error:', error.message)
+    }
+    console.error(error.config)
+    throw error // rethrow the error to propagate it
+  }
+}
+
 export const createSession = async (login, password) => {
   try {
     let url = '/session'
