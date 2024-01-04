6510a8ffb4b770a236a21e427aa1fdd45ede1244
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2024-01-04 00:43:16 -0300

    add dotenv

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: `http://localhost:${process.env.REACT_APP_PORT}`,
 })
