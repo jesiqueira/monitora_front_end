96a74577e674017dce9975276d671457c0d4ea43
Jose Edmar de Siqueira <edmar.ade@gmauil.com>
2024-01-10 17:13:18 -0300

    desenvolvendo pagina sobre site

--- src/Components/Site/SiteListar.jsx
+++ src/Components/Site/SiteListar.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Head from '../Helper/Head'
+import { ReactComponent as Site } from '../../Assets/site.svg'
+import { ReactComponent as AdmHome } from '../../Assets/add_home.svg'
+import styles from './SiteListar.module.css'
+import { getLocalSites } from '../../services/api/localSite'
 
 const SiteListar = () => {
+  
+  React.useEffect(() => {
+    const getSites = async () => {
+      const response = await getLocalSites()
+      console.log(response)
+    }
+    getSites()
+  }, [])
   return (
     <>
-      <Head title='Sites' description="Lista de todos os sites cadastrados." />
-      <h1>Lista de todos os sites</h1>
-      <Link to="cadastro"> Novo Site</Link>
+      <Head title="Sites" description="Lista de todos os sites cadastrados." />
+      <main className={styles.principal}>
+        <section className={styles.titulo}>
+          <div className={styles.nomeSite}>
+            <Site />
+            <h1>Adminitrar sites</h1>
+          </div>
+          <div className={styles.buscarSite}>
+            <AdmHome />
+            <Link to="cadastro"> Novo Site</Link>
+            <form className={styles.form}>
+              <input type="text" placeholder="buscar site" />
+              <button>Localizar</button>
+            </form>
+          </div>
+        </section>
+        <section className={styles.tabela}>
+          <hr />
+          <div className={styles.bgTable}>
+            <h1>Tabela aqui..</h1>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
