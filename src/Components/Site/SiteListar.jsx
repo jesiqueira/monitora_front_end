71515623643488c907eef2fe8cdb1ea28b1b7372
José Edmar de Siqueira <edmar.ade@gmail.com>
2024-01-23 13:09:00 -0300

    finalizado filtro de busca

--- src/Components/Site/SiteListar.jsx
+++ src/Components/Site/SiteListar.jsx
@@ -8,18 +8,34 @@ import { getLocalSites } from '../../services/api/localSite'
 import Table from './SiteTable'
 
 const SiteListar = () => {
-  const [site, setSite] = React.useState('')
+  const [sites, setSites] = React.useState('')
+  const [erro, setErro] = React.useState('')
 
   React.useEffect(() => {
     const getSites = async () => {
       const response = await getLocalSites()
       // console.log(response)
       if (response.status === 200) {
-        setSite(response.data)
+        setSites(response.data)
       }
     }
     getSites()
   }, [])
+
+  function handleBuscar(event) {
+    event.preventDefault()
+    if (event.target[0].value) {
+      const site = sites.filter((item) => item.nome.toLowerCase().includes(event.target[0].value.toLowerCase()))
+      if (site) {
+        setErro('')
+        setSites(site)
+      } else {
+        setErro('Site / local não localizado, verifique nome')
+      }
+    } else {
+      setErro('Preencher Campo de busca..')
+    }
+  }
   return (
     <>
       <Head title="Sites" description="Lista de todos os sites cadastrados." />
@@ -33,15 +49,16 @@ const SiteListar = () => {
             <Link to="cadastro">
               <AdmHome /> Novo Site
             </Link>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleBuscar}>
               <input type="text" placeholder="buscar site" />
               <button>Localizar</button>
             </form>
           </div>
+          <div className={styles.erro}>{erro && <p>{erro}</p>}</div>
         </section>
         <section className={styles.tabela}>
           <hr />
-          <Table sites={site} />
+          <Table sites={sites} />
           {/* <div className={styles.bgTable}><h1>Tabela aqui</h1></div> */}
         </section>
       </main>
