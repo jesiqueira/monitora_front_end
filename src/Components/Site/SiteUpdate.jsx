98a5abff2adbc7826406258bd3e7ca84e41a0ba9
Jose Edmar de Siqueira <edmar.ade@gmauil.com>
2024-01-24 12:28:10 -0300

    finalizado css para página update site / local

--- src/Components/Site/SiteUpdate.jsx
+++ src/Components/Site/SiteUpdate.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import styles from './SiteUpdate.module.css'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import { ReactComponent as Home } from '../../Assets/apartment.svg'
+import Head from '../Helper/Head'
 
 function SiteUpdate() {
   return (
-    <div>
-      <h1>Site Update</h1>
-    </div>
+    <main className={styles.main}>
+      <Head title="Sites-Update" description="Atualizar / excluir site cadastrado." />
+      <h1 className={styles.titulo}>Update / Delete - Localidade</h1>
+      <hr className={styles.hr} />
+      <section className={styles.section}>
+        <div className={styles.imagem}>
+          <Home />
+        </div>
+        <form className={styles.form}>
+          <Input label="Nome da Localidade" />
+          <div className={styles.dadosForm}>
+            <Input label="CEP" />
+            <Input label="Estado" />
+          </div>
+          <Input label="Rua" />
+          <div className={styles.dadosForm}>
+            <Input label="Número" />
+            <Input label="CNPJ" />
+          </div>
+          <Input label="Bairro" />
+          <div className={styles.button}>
+            <Button>Atualizar</Button>
+            <Button>Excluir</Button>
+          </div>
+        </form>
+      </section>
+    </main>
   )
 }
 
