e56bf88ae8b37f435c173d34d0c2744d57ab58d3
Jose Edmar de Siqueira <edmar.ade@gmauil.com>
2024-02-27 13:12:23 -0300

    Modificado Layout da page equipamentos

--- src/Components/Equipamento/EquipamentoListar.jsx
+++ src/Components/Equipamento/EquipamentoListar.jsx
@@ -3,9 +3,12 @@ import styles from './Equipamento.module.css'
 import Input from '../Forms/Input'
 import Select from '../Forms/Select'
 import Button from '../Forms/Button'
+import { ReactComponent as DATABASE } from '../../Assets/databases.svg'
+import { ReactComponent as SIGNAL } from '../../Assets/signal.svg'
 import { ReactComponent as ADD } from '../../Assets/add_circle.svg'
 import { ReactComponent as PDF } from '../../Assets/pdf.svg'
 import { ReactComponent as EXCELL } from '../../Assets/excell.svg'
+
 import { Link } from 'react-router-dom'
 
 const EquipamentoListar = () => {
@@ -25,41 +28,45 @@ const EquipamentoListar = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>EQUIPAMENTO EM - ESTOQUE</h1>
         <div className={styles.menu}>
-          <form className={styles.form}>
-            <div className={styles.inputRadio}>
-              <label htmlFor="estoque">
-                <input type="radio" name="local" id="estoque" value="estoque" defaultChecked />
-                Estoque
-              </label>
-              <label htmlFor="site">
-                <input type="radio" name="local" id="site" value="site" />
-                Site / Home
-              </label>
-              <label htmlFor="emprestimo">
-                <input type="radio" name="local" id="emprestimo" value="emprestimo" />
-                Emprestimo
-              </label>
+          <div className={styles.titulo}>
+            <DATABASE />
+            <h1>EQUIPAMENTO EM - ESTOQUE</h1>
+          </div>
+          <div className={styles.menu_item}>
+            <span>
+              <SIGNAL /> Status Dos Equipamentos
+            </span>
+            <Link to="equipamentoCadastro">
+              <ADD /> Adicionar
+            </Link>
+            <div className={styles.exportar}>
+              <PDF />
+              <EXCELL />
             </div>
-            <div className={styles.buscar}>
-              <Input placeholder="Buscar por....." />
-              <Select select={select} setSelect={setSelect} locais={opcao} />
-              <Button>Buscar</Button>
-            </div>
-            <div className={styles.icons}>
-              <Link to="">
-                <ADD />
-              </Link>
-
-              <div className={styles.exportar}>
-                <span>Exportar</span>
-                <PDF />
-                <EXCELL />
-              </div>
-            </div>
-          </form>
+          </div>
         </div>
+        <form className={styles.form}>
+          <div className={styles.inputRadio}>
+            <label htmlFor="estoque">
+              <input type="radio" name="local" id="estoque" value="estoque" defaultChecked />
+              Estoque
+            </label>
+            <label htmlFor="site">
+              <input type="radio" name="local" id="site" value="site" />
+              Site / Home
+            </label>
+            <label htmlFor="emprestimo">
+              <input type="radio" name="local" id="emprestimo" value="emprestimo" />
+              Emprestimo
+            </label>
+          </div>
+          <div className={styles.buscar}>
+            <Input placeholder="Buscar por....." />
+            <Select select={select} setSelect={setSelect} locais={opcao} />
+            <Button>Buscar</Button>
+          </div>
+        </form>
         <div className={styles.lista}>
           <p>Carregando Dados..........</p>
         </div>
